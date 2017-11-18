ba4c3fef8ab7b2b0904c7370ea56e0c1ed5c361c
Siva <siva@Admins-MacBook-Pro.local>
2017-11-18 16:14:39 +0530

    Optmizing scripts

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"bufio"
 	"encoding/csv"
+	"bytes"
+	"io"
 )
 
 func main() {
@@ -34,13 +36,13 @@ func main() {
 	// Start reading from the file with a reader.
 	reader := bufio.NewReader(file)
 
-	outputfile1, err := os.Create("text2.txt")
-	if(err!=nil){
-		fmt.Println("Not able to create a file")
-	}
-	defer outputfile1.Close()
+	//outputfile1, err := os.Create("text2.txt")
+	//if(err!=nil){
+	//	fmt.Println("Not able to create a file")
+	//}
+	//defer outputfile1.Close()
 
-	csvfile1, err := os.Create("result2.csv")
+	csvfile1, err := os.Create("resultPlatformDtl.csv")
 	if(err!=nil){
 		fmt.Println("Not able to create a csv file")
 	}
@@ -52,27 +54,40 @@ func main() {
 
 	limiter := time.Tick(time.Nanosecond * 333333333)
 
-	var line string
 	var linecount int
 	for {
 		linecount =0
 		var userdetails platformUserDetails
+		lines := make([]string, 2000)
 		query := ""
 		for linecount<1000 {
-			line, err = reader.ReadString('\n')
+			var buffer bytes.Buffer
+			var line []byte
+			line, _, err = reader.ReadLine()
+			buffer.Write(line)
+			println(buffer.String())
+			// If we're just at the EOF, break
 			if err != nil {
-				break
-			}
-			uid :=line[0:16]
-			if linecount == 0 {
-				query = query + "\"" + strings.TrimSpace(uid) + "\""
+				if query=="" {
+					fmt.Println("Final Number of records exported from the DB",recordsCount)
+					os.Exit(1)
+				} else {
+					break
+				}
 			} else {
-				query = query + ",\"" + strings.TrimSpace(uid) + "\""
+				uidString := string(line[:])
+				uid :=uidString[0:16]
+				lines = append(lines,uid)
+				if linecount == 0 {
+					query = query + "\"" + strings.TrimSpace(uid) + "\""
+				} else {
+					query = query + ",\"" + strings.TrimSpace(uid) + "\""
+				}
+				linecount++
 			}
-			linecount++
 		}
-		<-limiter
 
+		<-limiter
 		rows2,err := dbConn.Query("select * from platform_user_details where  hike_uid in ("+query+")")
 		if(err!=nil){
 			fmt.Println(err)
@@ -83,6 +98,7 @@ func main() {
 			err := rows2.Scan(&userdetails.ID,&userdetails.HikeUID, &userdetails.Msisdn, &userdetails.Name,
 				&userdetails.Gender,&userdetails.Circle, &userdetails.CreateTime, &userdetails.UpdateTime)
 			fmt.Println(err)
+
 			userDetailCreateTime := strings.Split(userdetails.CreateTime.String(),"+")
 			userDtlCrTime := userDetailCreateTime[0]
 
@@ -117,9 +133,9 @@ func main() {
 				}
 			}
 
-			outputfile1.WriteString(ToIntegerVal(count2)+"::"+userdetails.HikeUID+"::"+"+"+msisdnReqd2+"::"+ToString(
-				userdetails.Name)+"::"+ ToString(userdetails.Gender)+"::"+ToString(userdetails.Circle)+"::"+
-				"::"+strings.TrimSpace(userDtlCrTime)+"::"+strings.TrimSpace(userDtlUpTime)+"\n")
+			//outputfile1.WriteString(ToIntegerVal(count2)+"::"+userdetails.HikeUID+"::"+"+"+msisdnReqd2+"::"+ToString(
+			//	userdetails.Name)+"::"+ ToString(userdetails.Gender)+"::"+ToString(userdetails.Circle)+"::"+
+			//	"::"+strings.TrimSpace(userDtlCrTime)+"::"+strings.TrimSpace(userDtlUpTime)+"\n")
 		}
 
 		rows2.Close()
@@ -127,9 +143,9 @@ func main() {
 	}
 	fmt.Println("Final Number of records exported from the DB",recordsCount)
 
-	//if err != io.EOF {
-	//	fmt.Printf(" > Failed!: %v\n", err)
-	//}
+	if err != io.EOF {
+		fmt.Printf(" > Failed!: %v\n", err)
+	}
 
 }
 
